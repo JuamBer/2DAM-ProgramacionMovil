1a60e390c3f78b50f8a531cd1e15dc91c5c1dcb5
Juan Sáez García <contacto@juamber.com>
2022-01-12 19:09:26 +0100

    add 6,7

---
+++ 004.ListadosMenus/app/src/main/java/com/example/listadosmenus/Ejercicio6_6/Ejercicio6_6.java
@@ -0,0 +1,18 @@
+package com.example.listadosmenus.Ejercicio6_5;
+
+import android.os.Bundle;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import com.example.listadosmenus.R;
+
+public class Ejercicio6_5 extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.ejercicio6_5);
+    }
+
+
+}
