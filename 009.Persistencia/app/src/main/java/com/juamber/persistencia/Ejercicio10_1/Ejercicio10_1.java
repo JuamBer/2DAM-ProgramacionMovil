e2cd406405880ee54c32a70ca392ed3e4eeb280e
Juan Sáez García <contacto@juamber.com>
2022-02-08 12:04:40 +0100

    009.persistencia

---
+++ 009.Persistencia/app/src/main/java/com/juamber/persistencia/Ejercicio10_1/Ejercicio10_1.java
@@ -0,0 +1,13 @@
+package com.juamber.persistencia.Ejercicio10_1;
+
+import android.os.Bundle;
+import com.juamber.persistencia.R;
+import androidx.appcompat.app.AppCompatActivity;
+
+public class Ejercicio10_1 extends AppCompatActivity {
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.ejercicio10_1);
+    }
+}
---
+++ 009.Persistencia/app/src/main/java/com/juamber/persistencia/MainActivity.java
@@ -0,0 +1,39 @@
+package com.juamber.persistencia;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+
+public class MainActivity extends AppCompatActivity {
+
+    //EJERCICIO 1
+    private Button activity_main_btn_1;
+    //EJERCICIO 2
+
+    protected void cargarActivity(String activity) {
+        try {
+            Intent intent = new Intent(this, Class.forName(activity));
+            startActivity(intent);
+        } catch (ClassNotFoundException ex) {
+            System.out.println(ex.getMessage());
+        }
+    }
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        //EJERCICIO 1
+        this.activity_main_btn_1 = findViewById(R.id.activity_main_btn_1);
+        activity_main_btn_1.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                cargarActivity("com.juamber.persistencia.Ejercicio10_1.Ejercicio10_1");
+            }
+        });
+    }
+}
